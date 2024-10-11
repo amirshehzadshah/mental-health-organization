e79a37c2139bf34b086369c43952b7da00bd400e
Amir Shehzad Shah <140312408+amirshehzadshah@users.noreply.github.com>
2024-10-11 17:37:55 +0500

    add routes to navigate

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,6 +1,6 @@
 export const navigation = [
-    { id: 1, name: 'Home', href: '#', current: true },
-    { id: 2, name: 'Science', href: '#', current: false },
-    { id: 3, name: 'Benefits', href: '#', current: false },
-    { id: 4, name: 'Pricing', href: '#', current: false },
+    { id: 1, name: 'Home', href: '/', current: true },
+    { id: 2, name: 'Science', href: '/science', current: false },
+    { id: 3, name: 'Benefits', href: '/benefits', current: false },
+    { id: 4, name: 'Pricing', href: '/pricing', current: false },
   ]
