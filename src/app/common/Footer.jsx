bdd73c97e8f2632629b89c28cc42167599ed0b07
Amir Shehzad Shah <140312408+amirshehzadshah@users.noreply.github.com>
2024-10-14 17:40:22 +0500

    footer Css

--- src/app/common/Footer.jsx
+++ src/app/common/Footer.jsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import logoLight from '../../assets/Mental-Health-Logo-light.png'; // Import light mode logo
-import logoDark from '../../assets/Mental-Health-Logo-dark.png'; // Import dark mode logo
+import logo from '../../assets/Mental-Health-Logo.png';
 import { navigation } from '@/data/navigation';
 import { classNames } from '@/utils/classNames';
 import facebook from '../../assets/social/facebook.png'
@@ -11,33 +9,10 @@ import twitter from '../../assets/social/twitter.png'
 
 
 export default function Footer() {
-
-  const [logo, setLogo] = useState(logoLight);
-
-  useEffect(() => {
-    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-    // Set initial logo based on current theme
-    const setInitialLogo = (e) => {
-      setLogo(e.matches == 'true' ? logoLight : logoDark);
-    };
-
-    // Set initial logo on mount
-    setInitialLogo(prefersDarkScheme);
-
-    // Listen for changes in theme
-    prefersDarkScheme.addEventListener('change', setInitialLogo);
-
-    // Cleanup listener on component unmount
-    return () => {
-      prefersDarkScheme.removeEventListener('change', setInitialLogo);
-    };
-  }, []);
-
   return (
-    <footer className="footer w-full">
+    <footer className="footer w-full py-8 border">
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 space-y-10'>
-        <div className='relative flex max-sm:flex-col h-16 items-center justify-between'>
+        <div className='relative flex max-md:gap-4 max-sm:flex-col h-16 items-center justify-between'>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
               <Image src={logo} alt='logo' className="h-10 w-auto" />
@@ -60,11 +35,11 @@ export default function Footer() {
             ))}
           </div>
         </div>
-        <div className='relative flex max-sm:flex-col h-16 items-center justify-between'>
+        <div className='relative flex gap-4 max-sm:flex-col h-16 items-center justify-between'>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center gap-5">
-              <Image src={facebook} alt='logo' className="h-10 w-auto" />
-              <Image src={instagram} alt='logo' className="h-10 w-auto" />
+              <Image src={facebook} alt='logo' className="h-6 w-auto" />
+              <Image src={instagram} alt='logo' className="h-12 w-auto" />
               <Image src={twitter} alt='logo' className="h-10 w-auto" />
             </div>
           </div>
