a4fb6a93d9d9925844df082f61c49220d9ad4b5c
Amir Shehzad Shah <140312408+amirshehzadshah@users.noreply.github.com>
2024-10-16 20:19:07 +0500

    logo update

--- src/app/common/Footer.jsx
+++ src/app/common/Footer.jsx
@@ -2,21 +2,21 @@ import Image from 'next/image';
 import logo from '../../assets/Mental-Health-Logo.png';
 import { navigation } from '@/data/navigation';
 import { classNames } from '@/utils/classNames';
-import facebook from '../../assets/social/facebook.png'
-import instagram from '../../assets/social/instagram.png'
-import twitter from '../../assets/social/twitter.png'
+import facebook from '../../assets/social/Facebook.png'
+import instagram from '../../assets/social/Instagram.png'
+import twitter from '../../assets/social/Twitter.png'
 
 
 
 export default function Footer() {
   return (
-    <footer className="footer w-full py-8 border">
+    <footer className="footer w-full py-8">
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 space-y-10'>
-        <div className='relative flex max-md:gap-4 max-sm:flex-col h-16 items-center justify-between'>
+        <div className='relative flex max-md:gap-4 max-sm:flex-col h-16 items-center justify-between border'>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
               <Image src={logo} alt='logo' className="h-10 w-auto" />
-              <p className="font-semibold text-sm sm:text-lg md:text-2xl lg:text-4xl text-center max-w-32 sm:max-w-56 md:max-w-full">Mental Health Care</p>
+              <p className="font-semibold font-poppins text-sm sm:text-lg md:text-2xl lg:text-4xl text-center max-w-32 sm:max-w-56 md:max-w-full">Mental Health Care</p>
             </div>
           </div>
           <div className="flex space-x-4">
@@ -27,7 +27,7 @@ export default function Footer() {
                 aria-current={item.current ? 'page' : undefined}
                 className={classNames(
                   // item.current ? 'bg-gray-700 text-white' : 'hover:bg-gray-700 hover:text-white',
-                  'rounded-md px-3 py-2 text-sm font-medium',
+                  'rounded-md pl-3 py-2 text-sm font-medium',
                 )}
               >
                 {item.name}
@@ -35,11 +35,11 @@ export default function Footer() {
             ))}
           </div>
         </div>
-        <div className='relative flex gap-4 max-sm:flex-col h-16 items-center justify-between'>
+        <div className='relative flex gap-4 max-sm:flex-col h-16 items-center justify-between border'>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center gap-5">
-              <Image src={facebook} alt='logo' className="h-6 w-auto" />
-              <Image src={instagram} alt='logo' className="h-12 w-auto" />
+              <Image src={facebook} alt='logo' className="h-12 w-auto" />
+              <Image src={instagram} alt='logo' className="h-10 w-auto" />
               <Image src={twitter} alt='logo' className="h-10 w-auto" />
             </div>
           </div>
